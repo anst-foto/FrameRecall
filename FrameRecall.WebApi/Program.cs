0616059fa980ff630d58b171e3162b880126249e
Starinin Andrey <starinin-andrey@ya.ru>
2026-09-18 15:46:55 +0300

    Добавление проекта WebAPI

---
+++ FrameRecall.WebApi/Program.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.Run();
